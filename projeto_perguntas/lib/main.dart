3ee164f8a6358191ad30f4666be2008e7297c19a
Marrior <mrlimaroberto@gmail.com>
2024-03-17 20:32:00 -0300

    aprendendo básico de botões

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -10,9 +10,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    List<String> perguntas = ["pergunas1", "pergunta2"];
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(title: Text("Perguntas")),
-            body: Text("Olá FLutter")) /*Text("Olá Flutter")*/);
+            body: Column(children: [
+              Text("Olá FLutter"),
+              Text("Olá FLutter2"),
+              Text("Olá FLutter3"),
+              ElevatedButton(onPressed: null, child: Text("Resposta 1"))
+            ])));
   }
 }
