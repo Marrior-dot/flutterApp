61af7c18847a6fa627b5aeae3f6699a03ac2f53c
Marrior <mrlimaroberto@gmail.com>
2024-04-01 07:17:12 -0300

    api modificada

--- projeto_perguntas/lib/model/postagem.dart
+++ projeto_perguntas/lib/model/postagem.dart
@@ -4,29 +4,44 @@ class Postagem {
   final int likes;
   final int dislikes;
 
+/*
   const Postagem({
     required this.arquivo,
     required this.content,
     required this.likes,
     required this.dislikes,
-  });
+  });*/
 
-  factory Postagem.fromJson(Map<String, dynamic> json) {
+  Postagem(this.arquivo, this.content, this.likes, this.dislikes);
+
+  Postagem.fromJson(Map<String, dynamic> json)
+      : arquivo = json['arquivo'] as String,
+        content = json['content'] as String,
+        likes = json['likes'] as int,
+        dislikes = json['dislikes'] as int;
+
+  Map<String, dynamic> toJson() => {
+        'arquivo': arquivo,
+        'content': content,
+        'likes': likes,
+        'dislikes': dislikes,
+      };
+
+  /*factory Postagem.fromJson(Map<String, dynamic> json) {
     return switch (json) {
       {
         'arquivo': String arquivo,
         'content': String content,
         'likes': int likes,
         'dislikes': int dislikes,
-        
       } =>
         Postagem(
-            arquivo: arquivo,
-            content: content,
-            likes: likes,
-            dislikes: dislikes,
-            ),
+          arquivo: arquivo,
+          content: content,
+          likes: likes,
+          dislikes: dislikes,
+        ),
       _ => throw const FormatException('Falha ao carregar postagens'),
     };
-  }
+  }*/
 }
--- projeto_perguntas/lib/services/fetch.dart
+++ projeto_perguntas/lib/services/fetch.dart
@@ -6,14 +6,21 @@ import 'package:projeto_perguntas/model/postagem.dart' as postagem;
 Future<postagem.Postagem> fetchPostagem() async {
   final response =
       //ip de casa
-      //await http.get(Uri.parse('http://192.168.15.6:8000/api/postagemlist'));
-      //ip do estágio
-      await http.get(Uri.parse('http://10.54.2.110:8000/api/postagemlist'));
+      await http.get(Uri.parse('http://192.168.15.7:8000/api/postagemlist'));
+  //ip do estágio
+  //await http.get(Uri.parse('http://10.54.2.110:8000/api/postagemlist'));
+
   if (response.statusCode == 200) {
-    String nResponseBody=trimColchetes(response.body);
-    
-    return postagem.Postagem.fromJson(
-    jsonDecode(nResponseBody) as Map<String, dynamic>);
+    String nResponseBody = trimColchetes(response.body);
+    //print(nResponseBody);
+
+    //var postagemMap = jsonDecode(response.body) as Map<String, dynamic>;
+    var postagemMap = jsonDecode(nResponseBody) as Map<String, dynamic>;
+    var ppost = postagem.Postagem.fromJson(postagemMap[0]);
+
+    //return postagem.Postagem.fromJson(
+    //    json.decode(nResponseBody) as Map<String, dynamic>);
+    return ppost;
   } else {
     throw Exception('Failed to load album');
   }
@@ -22,7 +29,7 @@ Future<postagem.Postagem> fetchPostagem() async {
 String trimColchetes(String oldResponse) {
   String newResponseBody = "";
   RegExp exp = RegExp(r'(^\[)|(\]*)');
-  if (exp.hasMatch(oldResponse) == true){
+  if (exp.hasMatch(oldResponse) == true) {
     newResponseBody = oldResponse.replaceAll(exp, '').trim();
   }
   return newResponseBody;
