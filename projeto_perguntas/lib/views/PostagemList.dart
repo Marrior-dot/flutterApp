b0b84f9c4e3e4595d606050115869b916b8e1dae
Marrior <mrlimaroberto@gmail.com>
2024-05-08 14:08:20 -0300

    trabalhar na UI, Login, Registro e Postagem de lista

--- projeto_perguntas/lib/views/PostagemList.dart
+++ projeto_perguntas/lib/views/PostagemList.dart
@@ -19,7 +19,6 @@ class PostagemList extends StatefulWidget {
 class PostagemListState extends State<PostagemList> {
   late Future<List<Postagem>> futureFetch;
   late TextEditingController comentarioController;
-  //TextEditingController comentarioController = TextEditingController();
   String commentText="";
 
   @override
@@ -33,14 +32,6 @@ class PostagemListState extends State<PostagemList> {
     comentarioController.dispose();
     super.dispose();
   }
-
-  //List<TextEditingController> controllersComments(int quantidade){
-  //  List<TextEditingController> listaControllers=[];
-  //  for (int i = 0; i < quantidade; i++) {
-  //    listaControllers.add(TextEditingController());
-  //  }
-  //  return listaControllers;
-  //}
 
   TextEditingController controllerComments(){
     return TextEditingController();
@@ -68,158 +59,107 @@ class PostagemListState extends State<PostagemList> {
                             itemCount: snapshot.data!.length,
                             itemBuilder: (context, index) {
                               var likeButton = snapshot.data![index].likes;
-                              late var postagem = snapshot.data![index];
-                              //comentarioController = controllersComments(snapshot.data!.length)[index];
+                              var postagem = snapshot.data![index];
                               comentarioController = controllerComments();
+
                               return SizedBox(
-                                width: MediaQuery.of(context).size.width*0.5,
-                                height:MediaQuery.of(context).size.height*0.9, //200,
-                                child: Column(children: [
-                                  Text(snapshot.data![index].content),
-                                  Row(
-                                  children: [
-                                    //Botão para mudar os dislikes no feed de postagem, somente manda para o servidor
-                                    ElevatedButton(
-                                      onPressed: () {
-                                        updateLikeDislike(
-                                            'dislikes',
-                                            snapshot.data![index].dislikes,
-                                            snapshot.data![index].id,
-                                            snapshot.data![index].content);
-                                      },
-                                      child: const Text('dislikes'),
-                                    ),
-
-                                    //Botão para os likes no feed de postagem, atualiza o estado da tela e manda para o servidor
-                                    ElevatedButton(
-                                        onPressed: () {
-                                          setState(() {
-                                            likeButton = likeButton + 1;
-                                            updateLikeDislike(
-                                                'likes',
-                                                likeButton,
-                                                snapshot.data![index].id,
-                                                snapshot.data![index].content);
-                                          });
-                                        },
-                                        child: FutureBuilder<Postagem>(
-                                            future: fetchLike(
-                                                snapshot.data![index].id),
-                                            builder: (context, snapshot) {
-                                                        return Text(
-                                                  "likes ${snapshot.data!.likes}");
-                                            })),
-                                  ]),
-                                  TextFormField(
-                                      decoration: const InputDecoration(
-                                        labelText: 'Envie um Comentário',
-                                        prefixIcon: Icon(Icons.comment),
-                                      ),
-                                      controller: comentarioController,
-                                      onChanged: (value) => {
-                                          commentText = value,
-                                      },
-                                      ),
-                                  ElevatedButton(
-                                      onPressed: () {
-                                        setState(() {
-                                          createComment(
-                                              widget.user.username,
-                                              commentText,
-                                              postagem);
-                                        });
-                                      },
-                                      child: const Text("Enviar comentário")),
-                                  FutureBuilder<List<CommentsPostagem>>(
-                                    future: fetchComments(postagem),
-                                    builder: (context, snapshot) {
-                                      if (snapshot.connectionState ==
-                                          ConnectionState.waiting) {
-                                        return Center(
-                                          child: CircularProgressIndicator(
-                                            color: Theme.of(context)
-                                                .primaryColor, // Use theme color for consistency
-                                          ),
-                                        );
-                                      }
-
-                                      if (snapshot.hasError) {
-                                        return Center(
-                                          child: Text(
-                                            'Error: ${snapshot.error}',
-                                            style: TextStyle(
-                                                color: Colors
-                                                    .red), // Highlight error text
-                                          ),
-                                        );
-                                      }
-
-                                      if (!snapshot.hasData) {
-                                        // Handle empty state (no comments)
-                                        return Center(
-                                          child: Text(
-                                            'No comments found.',
-                                            style: TextStyle(
-                                                fontStyle: FontStyle.italic,
-                                                color: Colors
-                                                    .blueGrey), // Subtle blue-gray text
-                                          ),
-                                        );
-                                      }
-
-                                      final comments =
-                                          snapshot.data!; // Unwrap data safely
-
-                                      return ListView.separated(
-                                        // Use ListView.separated for built-in separator
-                                        shrinkWrap:
-                                            true, // Optimize for content size
-                                        itemCount: comments.length,
-                                        separatorBuilder: (context, index) =>
-                                            const Divider(
-                                          color: Colors
-                                              .blueGrey, // Light blue separator
-                                          height: 200, // Set separator height
-                                        ),
-                                        itemBuilder: (context, index) {
-                                          final comment = comments[index];
-
-                                          return Padding(
-                                            padding: const EdgeInsets.symmetric(
-                                                horizontal: 5.0,
-                                                vertical:
-                                                    5.0), // Increased spacing
-                                            child: Row(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment
-                                                      .start, // Align comment text
-                                              children: [
-                                                const SizedBox(
-                                                    width:
-                                                        8.0), // Spacing between avatar and text
-                                                Expanded(
-                                                  child: Text(
-                                                    comment.text,
-                                                    style: TextStyle(
-                                                      fontSize:
-                                                          16.0, // Adjust font size as needed
-                                                      color: Colors.blueGrey[
-                                                          800], // Darker blue text
-                                                    ),
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                          );
-                                        },
-                                      );
-                                    },
-                                  ),
-                                ]),
+                                width: MediaQuery.of(context).size.width*0.4,
+                                height:MediaQuery.of(context).size.height*0.5, //200,
+                                child: Column(children: 
+                                [Text(snapshot.data![index].content),
+Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: <Widget>[
+    Flexible(
+      child: ElevatedButton(
+        onPressed: () {
+          updateLikeDislike(
+              'dislikes',
+              snapshot.data![index].dislikes,
+              snapshot.data![index].id,
+              snapshot.data![index].content);
+        },
+        child: const Text('dislikes'),
+      ),
+    ),
+    Flexible(
+      child: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              likeButton = likeButton + 1;
+              updateLikeDislike(
+                  'likes',
+                  likeButton,
+                  snapshot.data![index].id,
+                  snapshot.data![index].content);
+            });
+          },
+          child: FutureBuilder<Postagem>(
+              future: fetchLike(snapshot.data![index].id),
+              builder: (context, snapshot) {
+                return Text(
+                  "likes ${snapshot.data!.likes}");
+              })),
+    ),
+  ],
+),
+Flexible(
+  child: TextFormField(
+    decoration: const InputDecoration(
+      contentPadding: EdgeInsets.all(5),
+      labelText: 'Escreva seu comentário',
+      prefixIcon: Icon(Icons.comment),
+    ),
+    controller: comentarioController,
+    onChanged: (value) => commentText = value,
+  ),
+),
+ElevatedButton(
+  onPressed: () {
+    setState(() {
+      createComment(
+          widget.user.username,
+          commentText,
+          postagem);
+    });
+  },
+  child: const Text("Enviar comentário")),
+Flexible(
+  child: FutureBuilder<List<CommentsPostagem>>(
+    future: fetchComments(postagem),
+    builder: (context, snapshot) {
+      if (snapshot.hasData) {
+          return Flexible(
+      child: ListView.separated(
+        itemCount: snapshot.data!.length,
+        separatorBuilder: (context, index) => const Divider(
+          color: Colors.grey,
+          thickness: 1.0, // Adjust divider thickness as needed
+        ),
+        itemBuilder: (context, index) {
+          final item = snapshot.data![index].text;
+          return ListTile(
+            title: Text(item),
+            trailing: Icon(Icons.arrow_right), // Add a trailing icon
+          );
+        },
+      ),
+    );
+        }
+      return Text('${snapshot.error}');
+    },
+  ),
+),
+]),
                               );
-                            });
+                            }
+                          );
                       }
                       return Text('${snapshot.error}');
-                    }))));
+                    }
+                  )
+                )
+              )
+            );
   }
 }
